d1c5eb68320fd0737f77d023442de3ddc48d0fd2
Agustin Alexander <aalexander@campus.ungs.edu.ar>
2019-07-31 12:12:42 -0700

    Added PairSum Exercise

---
+++ test/PairSumTest.java
@@ -0,0 +1,19 @@
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+public class PairSumTest {
+
+    @Test
+    public void pairSum(){
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(7);
+        list.add(8);
+        Assert.assertEquals(4,PairSum.pairSum(10,list));
+    }
+}
