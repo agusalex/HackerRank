4a8687ef746c600ceebb0b52f4df0d42c10ed559
Furkan Kahveci <33764493+furkankahvecii@users.noreply.github.com>
2019-08-25 12:48:02 +0300

    Create BigSorting.java

---
+++ src/main/java/Algorithms/Sorting/BigSorting.java
@@ -0,0 +1,22 @@
+/*
+ * https://www.hackerrank.com/challenges/big-sorting/problem
+*/
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+public class BigSorting
+{
+	  static String[] bigSorting(String[] unsorted) 
+    {
+      Arrays.sort(unsorted, (x, y) -> x.length() == y.length() ? x.compareTo(y) : Integer.compare(x.length(), y.length()));
+      return unsorted;
+    }
+}
