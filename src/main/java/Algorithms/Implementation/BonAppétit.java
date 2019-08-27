3241e3bbef163e3c1d6b37fdc6c5f5d89b8737d8
Furkan Kahveci <33764493+furkankahvecii@users.noreply.github.com>
2019-08-27 15:33:22 +0300

    Create BonAppétit.java

---
+++ src/main/java/Algorithms/Implementation/BonAppétit.java
@@ -0,0 +1,41 @@
+package Algorithms.Implementation;
+
+/*
+* https://www.hackerrank.com/challenges/bon-appetit/problem
+*/
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class BonAppétit
+{
+	static void bonAppetit(List<Integer> bill, int k, int b)
+	{
+		int toplam=0;
+        for(int i=0;i<n;i++)
+        {
+            if(i==k) continue;
+            else
+            {
+                toplam+=ar[i];
+            }
+        }
+
+        toplam=toplam/2;
+
+        if(toplam!=b)
+        {
+            System.out.println(Math.abs(toplam-b));
+        }
+        else
+        {
+            System.out.println("Bon Appetit");
+        }
+    }
+}
